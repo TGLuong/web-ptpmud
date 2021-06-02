1f538d4c72dfbf92d7f22d5cf8f45e3a6f073f15
TGLuong <gialuong9820@gmail.com>
2021-06-03 00:37:04 +0700

    dasda

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,9 +1,25 @@
 import {useState, useEffect, Fragment} from 'react'
-import {Row, Col} from 'antd'
-import {useLocation} from 'react-router-dom'
+import {Row, Col, Image} from 'antd'
+import {useLocation, Link} from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
 import '../css/Product.css'
 
+
+
+const AddTocartButton = styled.button`
+    height: 70px;
+    padding: 0px 15px;
+    border-radius: 18px;
+    background-color: #C0392D;
+    border: none;
+    &:focus{
+        outline: none;
+    }
+    &:hover{
+        background-color: #E0392D;
+    }
+`;
 
 function Product() {
     const [detail, setDetail] = useState({
@@ -17,7 +33,7 @@ function Product() {
             price:0.0,
             productName:'',
             productSummary:'',
-            quantity:'',
+            quantity:0,
             warranty:'',
         }
     })
@@ -39,33 +55,45 @@ function Product() {
         <Fragment>
             <Row className="product-root">
                 <Col xs={24}>
-                    <Row><h1>{detail.data.productName}</h1></Row>
-                    <Row>
-                        <Col md={10} className="img-section">
-                            <Row  align="middle" justify="center">
+                    <Row><h2><b>{detail.data.productName}</b></h2></Row>
+                    <Row justify="space-between">
+                        <Col md={9} className="img-section">
+                            <Row style  align="middle" justify="center">
                                 <Col className="main-img">
-                                    <img src={main_img} alt="main img" />
+                                    <Image className="img" src={main_img} />
                                 </Col>
                             </Row>
                             <div className="list-img" >
-                            
                                 {detail.data.images.map((element,index)=>{
                                     return(
-                                        <div key={index} className="img-container">
-                                            <img src={element} alt="adsd" />
+                                        <div onClick={()=>{set_main_img(element)}} key={index} className="img-container">
+                                            <img  src={element} alt="abc" />
                                         </div>
                                     );
-                                })}
+                                })}  
                             </div>
                         </Col>
                         <Col md={14} className="detail-section">
-                            <Row></Row>
-                            <Row></Row>
-                            <Row></Row>
-                            <Row></Row>
-                            <Row></Row>
-                            <Row></Row>
-                            <Row></Row>
+                            <Row>
+                                <AddTocartButton>
+                                    <Link to="/sign-in">
+                                        <div>
+                                            <h2 style={{color:'white',margin:'0px'}}>THÊM VÀO GIỎ HÀNG</h2>
+                                            <h4 style={{color:'white'}}>GIAO HÀNG NHANH CHÓNG</h4>
+                                        </div>
+                                    </Link>
+                                </AddTocartButton>
+                            </Row>
+                            <Row>Kho hàng: {detail.data.quantity}</Row>
+                            <Row><span style={{color:'red'}}>Giá bán: {detail.data.price} đ</span></Row>
+                            <Row><span style={{color:'red'}}>Bảo hành: {detail.data.warranty}</span></Row>
+                            <Row><span style={{color:'red'}}>Giao hàng miễn phí tận nơi, ưu đãi lắp đặt</span></Row>
+                            <Row><span style={{color:'red'}}>Bảo hành chính hãng ngay tại nhà</span></Row>
+                            {detail.data.productSummary.split('\n').map((element,index)=>{
+                                return(
+                                    <Row style={{margin:'0px'}} key={index}>{element}</Row>
+                                );
+                            })}
                         </Col>
                     </Row>
                 </Col>
