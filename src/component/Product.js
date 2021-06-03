b816325db64373b5cf3ff3cef44da0a35ff2b349
TGLuong <gialuong9820@gmail.com>
2021-06-03 23:49:43 +0700

    dsa

--- src/component/Product.js
+++ src/component/Product.js
@@ -9,6 +9,7 @@ import '../css/Product.css'
 
 const AddTocartButton = styled.button`
     height: 70px;
+    width:350px;
     padding: 0px 15px;
     border-radius: 18px;
     background-color: #C0392D;
@@ -77,15 +78,20 @@ function Product() {
                             <Row>
                                 <AddTocartButton>
                                     <Link to="/sign-in">
-                                        <div>
-                                            <h2 style={{color:'white',margin:'0px'}}>THÊM VÀO GIỎ HÀNG</h2>
-                                            <h4 style={{color:'white'}}>GIAO HÀNG NHANH CHÓNG</h4>
-                                        </div>
+                                        <Row style={{height:'100%'}} align="middle">
+                                            <Col md={18}>
+                                                <h4 style={{color:'white',margin:'0px'}}>THÊM VÀO GIỎ HÀNG</h4>
+                                                <h4 style={{color:'white'}}>GIAO HÀNG NHANH CHÓNG</h4>
+                                            </Col>
+                                            <Col md={6} >
+                                                <img style={{height:'100%', width:'100%'}} src="./img/core-img/shopping-cart.png"/>
+                                            </Col>
+                                        </Row>
                                     </Link>
                                 </AddTocartButton>
                             </Row>
                             <Row>Kho hàng: {detail.data.quantity}</Row>
-                            <Row><span style={{color:'red'}}>Giá bán: {detail.data.price} đ</span></Row>
+                            <Row><span style={{color:'red'}}>Giá bán: {detail.data.price} VNĐ</span></Row>
                             <Row><span style={{color:'red'}}>Bảo hành: {detail.data.warranty}</span></Row>
                             <Row><span style={{color:'red'}}>Giao hàng miễn phí tận nơi, ưu đãi lắp đặt</span></Row>
                             <Row><span style={{color:'red'}}>Bảo hành chính hãng ngay tại nhà</span></Row>
