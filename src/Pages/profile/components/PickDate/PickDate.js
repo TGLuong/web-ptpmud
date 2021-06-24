3a72c791163f03fa0654484aa12d9523ee5d4f8b
TGLuong <gialuong9820@gmail.com>
2021-06-24 22:09:41 +0700

    adsa

--- src/Pages/profile/components/PickDate/PickDate.js
+++ src/Pages/profile/components/PickDate/PickDate.js
@@ -1,146 +1,79 @@
 import {useEffect, useState} from 'react'
-import {Dropdown,Menu,Button,Row,Col} from 'antd'
-import {DownOutlined} from '@ant-design/icons'
-const {Item} = Menu;
-
+import {Row,Col,Select} from 'antd'
+const {Option} = Select
 const PickDate = props => {
     const [day,setDay] = useState(1)
     const [month,setMonth] = useState(1)
     const [year,setYear] = useState(new Date().getFullYear())
 
     const onChange = props.onChange || function(){}
+    const dayRender = () => {
+        let day = [];
+        if(month==2){
+            let maxDay = 0
+            if(year%4==0&&year%100!=0){
+                maxDay = 29
+            } else maxDay = 28
 
-    const dayOverLay = () => {
-        const renderItem = () =>{
-            let day = [];
-            if(month==2){
-                let maxDay = 0
-                if(year%4==0&&year%100!=0){
-                    maxDay = 29
-                } else maxDay = 28
-
-                for(let i=1;i<=maxDay;i++){
-                    day.push(<Item key={i}>{i}</Item>)
-                }
-            }else if(['1','3','5','7','8','10','12'].includes(month)){
-                for(let i=1;i<=31;i++){
-                    day.push(<Item key={i}>{i}</Item>)
-                }
-            }else{
-                for(let i=1;i<=30;i++){
-                    day.push(<Item key={i}>{i}</Item>)
-                }
+            for(let i=1;i<=maxDay;i++){
+                day.push(<Option key={i} value={i}>{i}</Option>)
             }
-            return day
+        }else if(['1','3','5','7','8','10','12'].includes(month)){
+            for(let i=1;i<=31;i++){
+                day.push(<Option key={i} value={i}>{i}</Option>)
+            }
+        }else{
+            for(let i=1;i<=30;i++){
+                day.push(<Option key={i} value={i}>{i}</Option>)
+            }
         }
-        return(
-            <Menu 
-                onClick={({item,key,keyPath,domEven})=>{setDay(key)}}
-            >
-                {renderItem()}
-            </Menu>
-        );
+        return day
     }
-    const monthOverLay = () => {
-        const monthRender = () => {
-            let month = [];
-            for(let i=1;i<=12;i++){
-                month.push(<Item key={i}>{i}</Item>);
-            }
-            return(month)
+    const monthRender = () => {
+        let month = [];
+        for(let i=1;i<=12;i++){
+            month.push(<Option key={i} value={i}>{i}</Option>);
         }
-        return(
-            <Menu
-                onClick={({item,key,keyPath,domEven})=>{
-                    if(key===2&&day>29){
-                        setMonth(key);
-                        setDay(29);
-                    }else if(key in [4,6,9,11]&&day>30){
-                        setMonth(key);
-                        setDay(30)
-                    }else{
-                        setMonth(key)
-                    }
-                    // onChange(day,month,year)
-                }}
-            >
-                {monthRender()}
-            </Menu>
-        )
+        return(month)
     }
-    const yearOverLay = () => {
-        const yearRender = () =>{
-            let year = []
-            for(let i=new Date().getFullYear();i>=new Date().getFullYear()-50;i--){
-                year.push(<Item key={i}>{i}</Item>)
-            }
-            return year
+    const yearRender = () => {
+        let year = []
+        for(let i=new Date().getFullYear();i>=new Date().getFullYear()-50;i--){
+            year.push(<Option key={i} value={i}>{i}</Option>)
         }
-        return(
-            <Menu onChange={props.onChange(day,month,year)} onClick={({item,key,keyPath,domEven})=>{setYear(key)}}>
-                {yearRender()}
-            </Menu>
-        )
+        return year
     }
     useEffect(()=>{
         onChange(day,month,year)
-    },[year,month,day])
+    },[day,month,year])
     return(
         <>
             <Row>
                 <Col sm={5}>
-                    <Dropdown
-                        overlay={dayOverLay} 
-                        trigger={['click']} 
-                        overlayStyle={{
-                            border:'1px solid #CFD8DC',
-                            position:'absolute',
-                            maxHeight:'200px',
-                            width:'100px',
-                            overflowX:'hidden',
-                            overflowY:'auto',
-                        }}
-                    >
-                        <Button style={{width:'100px'}}>
-                            {day}<DownOutlined style={{marginLeft:'50px'}} />
-                        </Button>
-                    </Dropdown>
+                    <Select onChange={(value,option)=>{setDay(value)}} style={{width:'100px'}} value={day}>
+                        {dayRender()}
+                    </Select>
                 </Col>
                 <Col md={5}>
-                    <Dropdown
-                        overlay={monthOverLay} 
-                        trigger={['click']} 
-                        overlayStyle={{
-                            border:'1px solid #CFD8DC',
-                            position:'absolute',
-                            maxHeight:'200px',
-                            width:'100px',
-                            overflowX:'hidden',
-                            overflowY:'auto',
+                    <Select 
+                        onChange={(value,option)=>{
+                            if(value===2&&day>28){
+                                if(year%4==0&&year%100!=0&&day>29){
+                                    setDay(29)
+                                }else setDay(28)
+                                
+                            }else if (value in [4,6,9,11]&&day>30) setDay(30)
+                            setMonth(value)
                         }}
+                        style={{width:'100px'}} value={month}
                     >
-                        <Button style={{width:'100px'}}>
-                            {month}<DownOutlined style={{marginLeft:'50px'}} />
-                        </Button>
-                    </Dropdown>
+                        {monthRender()}
+                    </Select>
                 </Col>
                 <Col md={5}>
-                    <Dropdown
-                        overlay={yearOverLay} 
-                        trigger={['click']} 
-                        overlayStyle={{
-                            border:'1px solid #CFD8DC',
-                            position:'absolute',
-                            maxHeight:'200px',
-                            width:'100px',
-                            overflowX:'hidden',
-                            overflowY:'auto',
-                        }}
-                    >
-                        <Button style={{width:'100px'}}>
-                            {year}<DownOutlined style={{marginLeft:'30px'}} />
-                        </Button>
-                    </Dropdown>
+                    <Select style={{width:'100px'}} value={year}>
+                        {yearRender()}
+                    </Select>
                 </Col>
             </Row>
             
