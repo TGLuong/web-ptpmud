855d4c692bc7db5a303f3a00568047b977383b17
TGLuong <gialuong9820@gmail.com>
2021-07-01 17:50:38 +0700

    first commit

--- src/Pages/profile/components/Password/Password.js
+++ src/Pages/profile/components/Password/Password.js
@@ -1,5 +1,10 @@
 
+import {
+    Card
+} from 'antd'
+import {
 
+} from '@ant-design/icons'
 
 
 
@@ -7,6 +12,29 @@ const Password = props => {
     return(
         <>
             <h1>Đổi Mật Khẩu</h1>
+            <Card
+                hoverable
+                style={{
+                    cursor:'context-menu'
+                }}
+            >
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="label">Nhập mật khẩu</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td className="label">Mật khẩu mới</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td className="label">Nhập lại mật khẩu</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Card>
         </>
     );
 }
