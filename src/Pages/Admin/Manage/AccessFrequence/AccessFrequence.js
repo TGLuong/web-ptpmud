0caa10cd1f75490162da85358fc72da3125b7ab8
TGLuong <gialuong9820@gmail.com>
2021-07-14 22:20:27 +0700

    echarts

--- src/Pages/Admin/Manage/AccessFrequence/AccessFrequence.js
+++ src/Pages/Admin/Manage/AccessFrequence/AccessFrequence.js
@@ -1,10 +1,51 @@
-
-
+import ReactECharts from 'echarts-for-react';
+import {
+    useEffect,
+    useState
+} from 'react'
+import axios from 'axios'
+import { baseUrl } from '../../../../config';
 
 
 const AccessFrequence = props => {
+    const [label, setLabel] = useState([])
+    const [data, setData] = useState([])
+
+
+    useEffect(()=>{
+        axios({
+            method:"GET",
+            url:baseUrl+'/visitor'
+        }).then(res=>{
+            setLabel(Object.keys(res.data))
+            setData(Object.values(res.data))
+        })
+    },[])
     return(
-        <div style={{height:'420px'}}>
+        <div>
+            <h1 style={{fontWeight:500}}>Lưu lượng truy cập 7 ngày gần nhất</h1>
+            <ReactECharts
+                style={{
+                    height:'500px'
+                }}
+                option={
+                    {
+                        xAxis: {
+                            type: 'category',
+                            data: label
+                        },
+                        yAxis: {
+                            type: 'value'
+                        },
+                        series: [{
+                            data: data,
+                            type: 'line',
+                            smooth: true
+                        }]
+                    }
+                }
+                
+            />
         </div>
     )
 }
